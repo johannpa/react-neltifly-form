e195034a5acfb6e39e633c70a6375d4e77d771fd
johannpa <johann_paque@hotmail.fr>
2021-09-26 11:30:01 +0200

    add contact form

--- src/composants/ContactForm.js
+++ src/composants/ContactForm.js
@@ -3,8 +3,27 @@ import "./ContactForm.css";
 
 function ContactForm() {
     return (
-        <div>
-            form
+        <div className="contact">
+            <form 
+                name="contact" 
+                method="post" 
+                className="contact__form" 
+                data-netlify="true" 
+                onSubmit="submit"
+            >
+                <input type="hidden" name="form-name" value="contact" />
+                <label for="name">Nom: </label>
+                <input type="text" name="name"/>
+                
+                <label for="email">Email: </label>
+                <input type="email" name="email"/>
+            
+                <label for="message">Message: </label>
+                <textarea name="message"></textarea>
+                
+                <button type="submit">Envoyer</button>
+               
+            </form>
         </div>
     );
 }
