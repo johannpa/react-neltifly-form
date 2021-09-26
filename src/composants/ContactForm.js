7a5352bffc50ac25ecfdd7094f48a797b7b8883d
johannpa <johann_paque@hotmail.fr>
2021-09-26 10:49:56 +0200

    the biginnig of the project

---
+++ src/composants/ContactForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ContactForm.css";
+
+function ContactForm() {
+    return (
+        <div>
+            form
+        </div>
+    );
+}
+
+export default ContactForm;
